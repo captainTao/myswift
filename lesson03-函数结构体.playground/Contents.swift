3f0bbdd95355c99812096caa9bd1b0892f8d5610
captainTao <578628438@qq.com>
2017-11-23 22:52:06 +0800

    update...

--- lesson03-函数结构体.playground/Contents.swift
+++ lesson03-函数结构体.playground/Contents.swift
@@ -56,21 +56,23 @@ func minMax(array: [Int]) -> (min: Int, max: Int) {
     }
     return (currentMin, currentMax)
 }
+
 let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
 print("min is \(bounds.min) and max is \(bounds.max)")
 // 打印 "min is -6 and max is 109"
 
 // 可选元组返回类型，返回可能为没有值
-func minMax(array: [Int]) -> (min: Int, max: Int)? {
-    if array.isEmpty { return nil }
-    var currentMin = array[0]
-    var currentMax = array[0]
-    for value in array[1..<array.count] {
-        if value < currentMin {
-            currentMin = value
-        } else if value > currentMax {
-            currentMax = value
-        }
-    }
-    return (currentMin, currentMax)
-}
+//func minMax(array: [Int]) -> (min: Int, max: Int)? {
+//    if array.isEmpty { return nil }
+//    var currentMin = array[0]
+//    var currentMax = array[0]
+//    for value in array[1..<array.count] {
+//        if value < currentMin {
+//            currentMin = value
+//        } else if value > currentMax {
+//            currentMax = value
+//        }
+//    }
+//    return (currentMin, currentMax)
+//}
+
